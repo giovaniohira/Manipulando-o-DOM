b57b153c9dc6fad4983f9fe035e8e7926b7beff9
Giovani Ohira <giovaniohira@gmail.com>
2023-10-17 11:24:33 -0300

    js adicionado

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const botoes = document.querySelectorAll('.app__card-button');
 const musicaToggle = document.querySelector('#alternar-musica');
 const startPause = document.querySelector('#start-pause');
 const icon = document.querySelector('.app__card-primary-butto-icon');
+const timer = document.querySelector('#timer');
 
 const IniciarEPausarBT = document.querySelector('#start-pause span');
 const focoBt = document.querySelector('.app__card-button--foco');
@@ -17,25 +18,29 @@ const pauseE = new Audio('sons/pause.mp3');
 const beepE = new Audio('sons/beep.mp3');
 musica.loop = true;
 
-let segundosCorridos = 5;
+let segundosCorridos = 1500;
 let IntervaloId = null;
 
 focoBt.addEventListener('click', () => {
+    segundosCorridos = 1500;
     alteraContexto('foco');
     focoBt.classList.add('active');
 })
 
 curtoBt.addEventListener('click', () => {
+    segundosCorridos = 300;
     alteraContexto('descanso-curto');
     curtoBt.classList.add('active');
 })
 
 longoBt.addEventListener('click', () => {
+    segundosCorridos = 900;
     alteraContexto('descanso-longo');
-   longoBt.classList.add('active');
+    longoBt.classList.add('active');
 })
 
 function alteraContexto(contexto) {
+    mostraTempo()
     //Passa por cada botão removendo a classe 'active'.
     botoes.forEach(function(contexto) {
         contexto.classList.remove('active');
@@ -47,7 +52,7 @@ function alteraContexto(contexto) {
         case "foco":
             texto.innerHTML = `Otimize sua produtividade,<br>
             <strong class="app__title-strong">mergulhe no que importa.</strong>`;
-            
+
             break;
         case "descanso-curto":
             texto.innerHTML = `Que tal dar uma respirada?<br>
@@ -82,7 +87,7 @@ const contagemRegressiva = () => {
         return;
     }
     segundosCorridos -= 1;
-    console.log('Temporizador: ' + segundosCorridos);
+    mostraTempo();
 }
 
 startPause.addEventListener('click', iniciarEPausar)
@@ -106,3 +111,11 @@ function parar() {
     IniciarEPausarBT.textContent = 'Iniciar'
     icon.setAttribute('src', `imagens/play_arrow.png`)
 }
+
+function mostraTempo() {
+    const tempo = new Date(segundosCorridos * 1000);
+    const tempoFormatado = tempo.toLocaleTimeString('pt-br', {minute: '2-digit', second: '2-digit'})
+    timer.innerHTML = `${tempoFormatado}`;
+}
+
+mostraTempo();
